4b20a54f3f00862c85daa9a71747d5130457de1e
tamburyniarz <tamburyniarz@gmail.com>
2016-07-22 16:41:00 +0200

    Added coseSettingButton

--- core/src/com/mygdx/teleclicker/Screens/SettingsScreen.java
+++ core/src/com/mygdx/teleclicker/Screens/SettingsScreen.java
@@ -7,14 +7,31 @@ import com.mygdx.teleclicker.Core.Assets;
 import com.mygdx.teleclicker.Enums.AssetsEnum;
 import com.mygdx.teleclicker.Enums.ScreenEnum;
 import com.mygdx.teleclicker.Service.ScreenService;
+import com.mygdx.teleclicker.Service.SoundService;
+import com.mygdx.teleclicker.ui.CloseSettingsButton;
+import com.mygdx.teleclicker.ui.IClickCallback;
 
 /**
  * Created by Senpai on 22.07.2016.
  */
 public class SettingsScreen extends AbstractScreen {
+    private CloseSettingsButton closeButton;
+
     @Override
     public void buildStage() {
         initBgTexture();
+        initCloseButton();
+    }
+
+    private void initCloseButton() {
+        closeButton = new CloseSettingsButton(new IClickCallback() {
+            @Override
+            public void onClick() {
+                SoundService.getInstance().playClickSound();
+                ScreenService.getInstance().setScreen(ScreenEnum.GAMEPLAY);
+            }
+        });
+        addActor(closeButton);
     }
 
     @Override
---
+++ core/src/com/mygdx/teleclicker/ui/CloseSettingsButton.java
@@ -0,0 +1,37 @@
+package com.mygdx.teleclicker.ui;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.mygdx.teleclicker.TeleClicker;
+
+/**
+ * Created by Senpai on 22.07.2016.
+ */
+public class CloseSettingsButton extends Button {
+    private final int WIDTH = 42;
+    private final int HEIGHT = 42;
+
+    private final int START_X = TeleClicker.WIDTH-WIDTH;
+    private final int START_Y = TeleClicker.HEIGHT-HEIGHT;
+
+    public CloseSettingsButton(final IClickCallback callback) {
+        super(new ButtonStyle());
+        init(callback);
+    }
+
+    private void init(final IClickCallback callback) {
+        this.setWidth(WIDTH);
+        this.setHeight(HEIGHT);
+        this.setX(START_X);
+        this.setY(START_Y);
+
+        this.addListener(new ClickListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                callback.onClick();
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
+    }
+}
