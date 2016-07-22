918deef662476709dba9b71f6acb30bd459d99d2
tamburyniarz <tamburyniarz@gmail.com>
2016-07-22 08:43:41 +0200

    Added NOT dispose gameplayScreen and shopScreen

--- core/src/com/mygdx/teleclicker/Service/ScreenService.java
+++ core/src/com/mygdx/teleclicker/Service/ScreenService.java
@@ -12,6 +12,9 @@ public class ScreenService {
 
     private static ScreenService instance;
 
+    private AbstractScreen gameplayScreen;
+    private AbstractScreen shopScreen;
+
     private TeleClicker game;
 
     private ScreenService(){
@@ -25,15 +28,35 @@ public class ScreenService {
     public void SetScreen(ScreenEnum screenEnum){
         // Get current screen to dispose it
         Screen curentScreen = game.getScreen();
+        ScreenEnum currentScreenEnum = TeleClicker.getActualScreen();
 
-        // Show new Screen
-        AbstractScreen newScreen = screenEnum.getScreen();
-        newScreen.buildStage();
+        AbstractScreen newScreen = null;
+
+        switch (screenEnum){
+            case GAMEPLAY:
+                if(gameplayScreen == null){
+                    gameplayScreen = screenEnum.getScreen();
+                    gameplayScreen.buildStage();
+                }
+                newScreen = gameplayScreen;
+                break;
+            case SHOP:
+                if(shopScreen == null){
+                    shopScreen = screenEnum.getScreen();
+                    shopScreen.buildStage();
+                }
+                newScreen = shopScreen;
+                break;
+            default:
+                newScreen = screenEnum.getScreen();
+                newScreen.buildStage();
+                break;
+        }
         TeleClicker.setActualScreen(screenEnum);
         game.setScreen(newScreen);
 
         // Dispose old screen
-        if(curentScreen!=null){
+        if(curentScreen!=null && currentScreenEnum != ScreenEnum.GAMEPLAY && currentScreenEnum != ScreenEnum.SHOP){
             curentScreen.dispose();
         }
     }
